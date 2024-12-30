f2ce5cef666bad5baef73f667db88a59ed80fd5e
ishaaaa8 <itsisha4884@gmail.com>
2024-12-30 10:11:05 +0530

    card and header components

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -3,37 +3,117 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 // import logopic from "../assets/logo.jpeg";
-
+// import MenuItems from "./MenuItems";
 
 const Header = () => {
-    return (
-      <div
-        className={`fixed w-full text-white flex justify-between p-4 items-center z-10 ${
-          navbar ? "navbar-active" : "bg-transparent"
-        }`}
-      >
-        <div className="md:flex-[0.5] flex-initial justify-center items-center">
-          <h1 className="w-16 logo-text font-semibold text-[4rem] cursor-pointer">WorkHire</h1>
-        </div>
-  
-        <nav>
-          <div className="absolute right-6 md:hidden top-6 scale-150">
-            {/* <MenuOutlined
-              onClick={showMenu}
-              className="scale-150 cursor-pointer"
-            /> */}
-          </div>
-  
-          <ul className="hidden text-xl md:flex gap-8 p-6 uppercase bg-transparent">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-           
-          </ul>
-          
-        </nav>
-      </div>
-    );
+  const [active, setActive] = useState(false);
+  const [navbar, setNavbar] = useState(false);
+
+  const navigate = useNavigate();
+
+  const showMenu = () => {
+    setActive(!active);
   };
-  
-  export default Header;
+  const changeBackground = () => {
+    if (window.scrollY >= 80) {
+      setNavbar(true);
+    } else setNavbar(false);
+  };
+  window.addEventListener("scroll", changeBackground);
+  return (
+    <div
+      className={`fixed w-full text-white flex justify-between p-4 items-center z-10 ${
+        navbar ? "navbar-active" : "bg-transparent"
+      }`}
+    >
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <h1 className="w-16 logo-text font-semibold text-[4rem] cursor-pointer">WorkHire</h1>
+      </div>
+
+      <nav>
+        <div className="absolute right-6 md:hidden top-6 scale-150">
+          <MenuOutlined
+            onClick={showMenu}
+            className="scale-150 cursor-pointer"
+          />
+        </div>
+
+        <ul className="hidden text-xl md:flex gap-8 p-6 uppercase bg-transparent">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/privacy">Terms</Link>
+          </li>
+          {localStorage.getItem("user_token") ||
+          localStorage.getItem("admin_token") ? (
+            localStorage.getItem("user_token") ? (
+              <>
+                <li>
+                  <button
+                    className="uppercase"
+                    onClick={() => {
+                      navigate("/userdashboard");
+                    }}
+                  >
+                    Dashboard
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="uppercase"
+                    onClick={() => {
+                      localStorage.removeItem("user_token");
+                      navigate("/");
+                      window.location.reload();
+                    }}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <button
+                    className="uppercase"
+                    onClick={() => {
+                      navigate("/admindashboard");
+                    }}
+                  >
+                    Dashboard
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="uppercase"
+                    onClick={() => {
+                      localStorage.removeItem("admin_token");
+                      navigate("/");
+                      window.location.reload();
+                    }}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </>
+            )
+          ) : (
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          )}
+        </ul>
+        {/* <MenuItems showMenu={showMenu} active={active} /> */}
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "../components/header";
+import Navbar from "../components/navbar";
+import wave1 from "../assets/bg.jpg";
 
 export default function Home(){
+    const img1 = () => {
+        document.getElementById("hero").style.backgroundImage = `url(${wave1})`;
+        document.getElementById("hero").style.transition = "ease-in-out 1s";
+      };
+
     return(
-        <div>
-            <Header/>
+        <>
+            <Navbar/>
             <h1>Home Page</h1>
-        </div>
+        </>
     )
 } 
